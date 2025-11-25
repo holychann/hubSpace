ad3fab377e07057f2981dcb45f35acf8f3020a11
holychan <east0321@outlook.kr>
2025-11-26 00:38:37 +0900

    feat: 이벤트 도메인 레포지토리 생성

---
+++ src/main/java/com/example/backend/domain/event/repository/EventRepository.java
@@ -0,0 +1,10 @@
+package com.example.backend.domain.event.repository;
+
+import com.example.backend.domain.event.entity.EventEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<EventEntity, Long> {
+
+}
