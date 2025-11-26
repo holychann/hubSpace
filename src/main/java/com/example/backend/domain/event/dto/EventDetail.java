6be1951b360d4c9798750e49e5b5b6858de2085c
holychan <east0321@outlook.kr>
2025-11-26 12:50:48 +0900

    feat: 이벤트 도메인 응답 DTO 설계

--- src/main/java/com/example/backend/domain/event/dto/EventDetail.java
+++ src/main/java/com/example/backend/domain/event/dto/EventDetail.java
@@ -2,12 +2,15 @@ package com.example.backend.domain.event.dto;
 
 import com.example.backend.domain.event.entity.EventType;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EventDetail {
-    String getId();
+    Long getId();
+    Long getCount();
     String getEventTitle();
     Boolean getIsActive();
     EventType getEventType();
     List<String> getSearchColumns();
+    LocalDateTime getCreatedAt();
 }
--- src/main/java/com/example/backend/domain/event/dto/EventResponseDto.java
+++ src/main/java/com/example/backend/domain/event/dto/EventResponseDto.java
@@ -1,26 +1,66 @@
 package com.example.backend.domain.event.dto;
 
+import com.example.backend.domain.event.entity.EventType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 이벤트 도메인 API의 응답 DTO
+ */
 public class EventResponseDto {
 
     /**
      * 모든 이벤트 목록
      */
     public static class SearchList {
-        int count;
-
+        Integer count;
+        List<EventDetail> events;
     }
 
     /**
      * Google Form형 이벤트의 상세 정보
      */
-    public static class SearchFormDetail {
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class SearchFormDetail implements EventDetail{
+        // 공통 멤버
+        private Long id;
+        private String eventTitle;
+        private Boolean isActive;
+        private EventType eventType;
+        private List<String> searchColumns;
+        private LocalDateTime createdAt;
+        private Long count;
 
+        // Google Form형 이벤트의 멤버
+        private String formUrl;
     }
 
     /**
      * CSV형 이벤트의 상세 정보
      */
-    public static class SearchFileDetail {
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class SearchFileDetail implements EventDetail{
+        // 공통 멤버
+        private Long id;
+        private String eventTitle;
+        private Boolean isActive;
+        private EventType eventType;
+        private List<String> searchColumns;
+        private LocalDateTime createdAt;
+        private Long count;
 
+        // CSV형 이벤트의 멤버
+        private String displayColumn;
     }
 }
