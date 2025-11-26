2bd7ca7ef8325d658fd5c7a21933b49cfdd4dbf1
holychan <east0321@outlook.kr>
2025-11-26 20:41:57 +0900

    feat: 이벤트 도메인 커맨드 컨트롤러 구글 폼 생성 및 이벤트 생성 로직 구현

---
+++ src/main/java/com/example/backend/domain/event/controller/EventCommandController.java
@@ -0,0 +1,41 @@
+package com.example.backend.domain.event.controller;
+
+import com.example.backend.application.facade.EventFacade;
+import com.example.backend.domain.event.dto.EventRequestDto;
+import com.example.backend.domain.event.dto.EventResponseDto;
+import com.example.backend.global.dto.ApiResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/events")
+@RequiredArgsConstructor
+@Slf4j
+public class EventCommandController {
+
+    private final EventFacade eventFacade;
+
+    @PostMapping("/form")
+    public ResponseEntity<ApiResponseDto> createFormEvent(
+            @AuthenticationPrincipal OAuth2User user,
+            @RequestBody EventRequestDto.CreateFormEvent eventRequestDto
+    ) {
+        log.info("[EVENT][CTRL][REQUEST] /events/form POST start | userEmail: {}", user.getAttributes().get("email"));
+
+        long start = System.currentTimeMillis();
+
+        EventResponseDto.CreatedFormEvent formInfo = eventFacade.createFormEvent(user, eventRequestDto);
+
+        long end = System.currentTimeMillis();
+        long elapsed = end - start;
+
+        log.info("[EVENT][CTRL][RESPONSE] /events/form POST end | elapsed: {}", elapsed);
+
+        return ResponseEntity.ok(ApiResponseDto.success(formInfo));
+    }
+}
