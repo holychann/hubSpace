41bd610d5d8b0e60751ca6e4abc267b4ed0aa862
holychan <east0321@outlook.kr>
2025-11-26 12:38:56 +0900

    feat: 이벤트 디테일 인터페이스 추가

--- src/main/java/com/example/backend/domain/event/dto/EventDetail.java
+++ src/main/java/com/example/backend/domain/event/dto/EventDetail.java
@@ -1,6 +1,13 @@
 package com.example.backend.domain.event.dto;
 
+import com.example.backend.domain.event.entity.EventType;
+
+import java.util.List;
+
 public interface EventDetail {
     String getId();
     String getEventTitle();
+    Boolean getIsActive();
+    EventType getEventType();
+    List<String> getSearchColumns();
 }
