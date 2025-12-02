6de2094480bda4d4c0528a3a4d885b25dce2735d
holychan <east0321@outlook.kr>
2025-12-02 15:52:17 +0900

    refactor: 응답 조회 URI 구조 변경

--- src/main/java/com/example/backend/domain/response/controller/ResponseQueryController.java
+++ src/main/java/com/example/backend/domain/response/controller/ResponseQueryController.java
@@ -6,17 +6,13 @@ import com.example.backend.global.dto.ApiResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/response")
 public class ResponseQueryController {
 
     private final ResponseFacade responseFacade;
@@ -26,18 +22,13 @@ public class ResponseQueryController {
      * @param params : 답변 조회 조건
      * @return ApiResponseDto
      */
-    @GetMapping()
+    @GetMapping("/events/{eventId}/search")
     public ResponseEntity<ApiResponseDto> getResponse(
+            @PathVariable Long eventId,
             @RequestParam Map<String, String> params
             ){
 
-        /**
-         * 1️⃣ 이벤트 ID 파라미터 추출
-         * 2️⃣ 이벤트 ID 제거
-         * 3️⃣ 답변 조회 조건 DTO 생성
-         */
-        Long eventId = Long.parseLong(params.get("eventId"));
-        params.remove("eventId");
+        // DTO로 변환
         ResponseQueryDto queryDto = ResponseQueryDto.of(eventId, params);
 
         long start = System.currentTimeMillis();
--- src/main/java/com/example/backend/global/config/SecurityConfig.java
+++ src/main/java/com/example/backend/global/config/SecurityConfig.java
@@ -123,7 +123,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/user").hasRole(UserRoleType.USER.name())
                         .requestMatchers(HttpMethod.PUT, "/user").hasRole(UserRoleType.USER.name())
                         .requestMatchers(HttpMethod.DELETE, "/user").hasRole(UserRoleType.USER.name())
-                        .requestMatchers(HttpMethod.GET, "/response/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/events/*/search").permitAll()
                         .anyRequest().authenticated()
                 );
 
