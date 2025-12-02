7049a2196a5cae0cc06ab669c57e9af95247db9c
holychan <east0321@outlook.kr>
2025-12-02 16:29:10 +0900

    feat: 폴링 데이터 저장 로직 구현

---
+++ src/main/java/com/example/backend/domain/response/service/command/ResponseCommandService.java
@@ -0,0 +1,9 @@
+package com.example.backend.domain.response.service.command;
+
+import com.example.backend.infra.google.dto.GoogleFormResponseDto;
+
+import java.util.List;
+
+public interface ResponseCommandService {
+    void saveResponses(List<GoogleFormResponseDto> responses);
+}
